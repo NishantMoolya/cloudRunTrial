31b692ff5ba07d1183050e8a3ca4c2aec7b87c9f
NishantMoolya <nishantmoolya@gmail.com>
2024-11-03 16:05:34 +0530

    initial server setup completed

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/",(req,res)=>{
+    return res.status(200).json({message : "Cloud Run and Cloud Build Demo - CI CD Pipeline"})
+})
+
+app.get("/echo/:text",(req,res)=>{
+    const {text}  = req.params;
+    return res.status(200).json({message : "Echo Route",data : text})
+})
+
+app.listen(port,()=>{
+    console.log(`server listening on port ${port}`)
+});
